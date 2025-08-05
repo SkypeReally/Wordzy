16fd73ed149bbb2c4750060d32ced0c51875a5ba
SkypeReally <sunojcareulium@gmail.com>
2025-08-05 22:04:41 +0530

    changed Utils

--- lib/Util/debug_util.dart
+++ lib/Util/debug_util.dart
@@ -2,7 +2,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:intl/intl.dart';
 
 class DebugUtils {
-  /// Clears today's daily word "played" state for all word lengths (3–8)
   static Future<void> clearDailyWordPlayedKeys() async {
     final prefs = await SharedPreferences.getInstance();
     final today = DateFormat('yyyy-MM-dd').format(DateTime.now());
@@ -10,7 +9,6 @@ class DebugUtils {
     for (int length = 3; length <= 8; length++) {
       final key = 'daily_played_${length}_$today';
       await prefs.remove(key);
-      // Optional: print for debug
       print('🧹 Cleared: $key');
     }
   }
